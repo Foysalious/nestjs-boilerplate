23e03140035da3ba4dd6d949a7d90446572f30d7
foysal_tikweb <rahman@tikweb.com>
2022-11-30 18:05:29 +0600

    connect local db

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,13 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { registerAs } from '@nestjs/config';
 require('dotenv').config()
+// 'mongodb+srv://qwerty1234:qwerty1234@cluster0.7bupr.mongodb.net/attendence'
 
 export default registerAs(
     'typeOrmConfig',
     (): TypeOrmModuleOptions => ({
         type: 'mongodb',
-        url: 'mongodb+srv://qwerty1234:qwerty1234@cluster0.7bupr.mongodb.net/attendence',
+        url: 'mongodb://localhost:27017/attendence',
         entities: [__dirname + '/../**/*.entity.{js,ts}'],
         useUnifiedTopology: true 
     }),
-);
+); 
